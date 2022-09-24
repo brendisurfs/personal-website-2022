675e88f5204be36d24c5ffe31a792c1e2ada180c
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-09-23 17:32:46 -0700

    fix: fixed overflow noscroll
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- src/components/ThreeJS/ThreeRenderer.ts
+++ src/components/ThreeJS/ThreeRenderer.ts
@@ -11,15 +11,34 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.z = 1;
 
+const grid = new THREE.PlaneGeometry(2.0, 2.0);
 const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
 const material = new THREE.MeshNormalMaterial();
 
 const customGLSLMaterial = new THREE.RawShaderMaterial({
-  vertexShader: "",
-  fragmentShader: "",
+  vertexShader: `
+        uniform mat4 projectionMatrix;
+        uniform mat4 viewMatrix;
+        uniform mat4 modelMatrix;
+
+        attribute vec3 position;
+
+        void main()
+        {
+            gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+        }
+  `,
+  fragmentShader: `
+    precision mediump float;
+
+    void main()
+    {
+      gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+    }
+  `,
 });
 
-const mesh = new THREE.Mesh(geometry, material);
+const mesh = new THREE.Mesh(grid, customGLSLMaterial);
 scene.add(mesh);
 
 // RENDERER
@@ -32,6 +51,7 @@ export const renderCanvas = () => {
 function animation(time: number) {
   mesh.rotation.x = time / 2000;
   mesh.rotation.y = time / 1000;
+  renderer.render(scene, camera);
 }
 //
 // exports the render target to Rescript.
