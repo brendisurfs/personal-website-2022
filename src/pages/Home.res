a7cc88168809bf454e4e2adf9fa16e55fbe97a71
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-09-24 13:32:44 -0700

    fix: added resolver for wasm files
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- src/pages/Home.res
+++ src/pages/Home.res
@@ -1,5 +1,10 @@
 open Layout
 
+%%raw(`
+      import init from "@wasm/out-wasm/brendancreates-wasm.js";
+      init("@wasm/out-wasm/brendancreates-wasm_bg.wasm");
+`)
+
 @react.component
 let make = () => {
   /* <Layout> <div> {"Testing Three on Homepage"->React.string} </div> <ThreeCanvas /> </Layout> */
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
+  base: "./",
+  resolve: {
+    alias: {
+      "@wasm": path.resolve(__dirname, "./wasm"),
+    },
+  },
   define: {
     "process.env": process.env,
   },
-})
+});
