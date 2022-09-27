795abf8d789349e30025a81698313db34a23e0ae
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-09-27 16:13:40 -0700

    quick addition for my distortion shader
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- src/components/ThreeJS/ThreeRenderer.ts
+++ src/components/ThreeJS/ThreeRenderer.ts
@@ -35,8 +35,57 @@ const material = new MeshNormalMaterial();
 
 // custom overlay.
 const customGLSLMaterial = new ShaderMaterial({
-  vertexShader: "",
-  fragmentShader: "",
+  vertexShader: `
+precision mediump float;
+#pragma glslify: snoise = require("glsl-noise/simplex/3d")
+
+varying vec2 vUv;
+varying float vWave;
+uniform float uTime;
+uniform vec2 mousePos;
+
+void main() {
+  vUv = uv;
+
+  vec3 pos = position;
+  float noiseFreq = 30.0;
+  float noiseAmp = 0.1;
+  vec3 noisePos = vec3(pos.x * noiseFreq + uTime * 0.5, mousePos.x, mousePos.y);
+  pos.z += snoise(noisePos) * noiseAmp;
+  vWave = pos.z;
+
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
+  `,
+  fragmentShader: `
+varying vec2 vUv;
+varying float vWave;
+uniform vec2 uMouse;
+
+uniform sampler2D uTexture;
+uniform vec2 resolution;
+
+float circle(vec2 uv, vec2 disc_center, float disc_radius, float border_size)
+{
+  uv -= disc_center;
+  uv *= resolution;
+  float dist = sqrt(dot(uv, uv));
+  return smoothstep(disc_radius + border_size, disc_radius - border_size, dist);
+}
+
+void main() {
+  vec2 newUV = vUv;
+  float c = circle(vUv, uMouse, 0.0, 0.2);
+  float wave = vWave;
+
+  float r = texture2D(uTexture, newUV.xy += c * (wave * 0.5)).r;
+  float g = texture2D(uTexture, newUV.xy += c * (0.1 * 0.525)).g;
+  float b = texture2D(uTexture, newUV.xy += c * (0.1 * 0.55)).b;
+
+  vec3 texture = vec3(r, g, b);
+  gl_FragColor = vec4(texture, 1.);
+}
+`,
 });
 //
 const mesh = new Mesh(geometry, material);
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
+  plugins: [glslify()],
   define: {
     "process.env": process.env,
   },
